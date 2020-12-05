04b1972c4cbd181ddaaa25c947955a7a2cf16e48
tharindu4903 <tharindudhananjaya66@gmail.com>
2020-12-05 15:32:54 +0530

    edit

--- sendEmail.php
+++ sendEmail.php
@@ -6,20 +6,7 @@ if(isset($_POST['submit'])) {
    $subject = $_POST['contactSubject'];
    $contact_message = $_POST['contactMessage'];
 
-   // Check Name
-	if (strlen($name) < 2) {
-		$error['name'] = "Please enter your name.";
-	}
-	// Check Email
-	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
-		$error['email'] = "Please enter a valid email address.";
-	}
-	// Check Message
-	if (strlen($contact_message) < 15) {
-		$error['message'] = "Please enter your message. It should have at least 15 characters.";
-	}
-   // Subject
-	if ($subject == '') { $subject = "Contact Form Submission"; }
+
 
 	$to='tharindudhananjaya66@gmail.com'; // Receiver Email ID, Replace with your email ID
 	$headers="From: ".$name."\n"."email :".$email;
