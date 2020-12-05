bb4448624581cfb6d8e143439ca3f9514ba11ecd
tharindu4903 <tharindudhananjaya66@gmail.com>
2020-12-05 15:33:51 +0530

    form

--- sendEmail.php
+++ sendEmail.php
@@ -11,7 +11,7 @@ if(isset($_POST['submit'])) {
 	$to='tharindudhananjaya66@gmail.com'; // Receiver Email ID, Replace with your email ID
 	$headers="From: ".$name."\n"."email :".$email;
 
-	if(mail($to, $subject, $message, $headers)){
+	if(mail($to, $subject, $contact_message, $headers)){
 		echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
 	}
 	else{
