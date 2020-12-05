3cc3f743a5e7b081ec9dceb5acdccf8bf8ed4bcd
tharindu4903 <tharindudhananjaya66@gmail.com>
2020-12-05 15:24:17 +0530

    5

--- sendEmail.php
+++ sendEmail.php
@@ -1,9 +1,5 @@
 ﻿<?php
 
-// Replace this with your own email address
-$siteOwnersEmail = 'tharindudhananjaya66@gmail.com';
-
-
 if($_POST) {
 
    $name = trim(stripslashes($_POST['contactName']));
@@ -26,43 +22,15 @@ if($_POST) {
    // Subject
 	if ($subject == '') { $subject = "Contact Form Submission"; }
 
+	$to='tharindudhananjaya66@gmail.com'; // Receiver Email ID, Replace with your email ID
+	$headers="From: ".$name."\n"."Phone :".$email;
 
-   // Set Message
-   $message .= "Email from: " . $name . "<br />";
-	$message .= "Email address: " . $email . "<br />";
-   $message .= "Message: <br />";
-   $message .= $contact_message;
-   $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
-
-   // Set From: header
-   $from =  $name . " <" . $email . ">";
-
-   // Email Headers
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $email . "\r\n";
- 	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-mail($siteOwnersEmail,$subject,$contact_message,$headers);
-   if (!$error) {
-
-      ini_set("sendmail_from", $siteOwnersEmail); // for windows server
-      $mail = mail($siteOwnersEmail, $subject, $message, $headers);
-
-		if ($mail) { echo "OK"; }
-      else { echo "Something went wrong. Please try again."; }
-		
-	} # end if - no validation error
-
-	else {
-
-		$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
-		$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
-		$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
-		
-		echo $response;
-
-	} # end if - there was a validation error
+	if(mail($to, $subject, $message, $headers)){
+		echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+	}
+	else{
+		echo "Something went wrong!";
+	}
 
 }
 
